f1a4f6422093dfcc70a2ff54b0a3e84e5d8eca64
Daniel Machado <87205289+dmaxadu@users.noreply.github.com>
2024-10-13 10:07:26 -0300

    fix: corrige estilização

--- lumus-app/src/Components/CardComponent.js
+++ lumus-app/src/Components/CardComponent.js
@@ -8,8 +8,8 @@ const CardComponent = ({ image, title, description, color, onClick, isActive }) 
     <motion.div
       className="card"
       onClick={onClick}
-      initial={{ width: isActive ? '20%' : '1%', height: isActive ? '80%' : '80%', borderRadius: isActive ? '25px' : '25px' }}
-      animate={{ width: isActive ? '20%' : '1%', height: isActive ? '80%' : '80%', borderRadius: isActive ? '25px' : '25px' }}
+      initial={{ width: isActive ? '60vw' : '0.1vw', height: isActive ? '70vh' : '70vh', borderRadius: isActive ? '40px' : '40px' }}
+      animate={{ width: isActive ? '60vw' : '0.1vw', height: isActive ? '70vh' : '70vh', borderRadius: isActive ? '40px' : '40px' }}
       transition={{ duration: 0.5 }}
       style={{
         backgroundColor: color,
@@ -17,13 +17,12 @@ const CardComponent = ({ image, title, description, color, onClick, isActive }) 
         flexDirection: 'column',
         justifyContent: 'space-around',
         alignItems: 'center',
-        padding: '20px',
-        margin: '0 5px',
+        padding: '18px',
+        margin: '0 5px 15vh',
         cursor: 'pointer',
         position: 'relative',
         overflow: 'hidden',
         boxShadow: isActive ? '0px 4px 10px rgba(0, 0, 0, 0.2)' : 'none',
-        textAlign: 'left',
       }}
     >
       {isActive && (
