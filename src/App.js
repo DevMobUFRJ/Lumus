6364f52a0a4927c930563e3b4cc837d08ba756a5
Daniel Machado <87205289+dmaxadu@users.noreply.github.com>
2024-12-01 18:01:17 -0300

    correção de diretório

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import img3 from './Assets/Images/JEMA_GER_1639-09-removebg-preview 1.png';
 import imgtest from './Assets/Images/Union.png';
 import arrow from './Assets/Images/Vector.svg';
 import bg from './Assets/Images/Onboarding 1.png';
-import '../src/App.css';
+import './App.css';
 import HomePage from './Components/Home';
 import LuzGeral from './Components/LuzGeral';
 import Faq from './Components/Faq';
