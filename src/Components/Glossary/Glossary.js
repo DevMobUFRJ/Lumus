25d34127e805b4470ac7bf80cbaee2c9d26959f8
Pedro Mion <pedromionbraga@gmail.com>
2025-04-18 15:13:41 -0300

    add: textos glossario

--- src/Components/Glossary/Glossary.js
+++ src/Components/Glossary/Glossary.js
@@ -25,7 +25,7 @@ const Glossary = () => {
                     border='1px solid gray'
                     upperText='Fluxo Luminoso'
                     bottomText='Lúmen (Lm)'
-                    dropdownText='teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste '
+                    dropdownText='É a quantidade de luz emitida pelo equipamento. Deve ser ajustado à sensibilidade do olho humano para permitir a realização de tarefas sem causar ofuscamento. O cálculo do fluxo luminoso depende das dimensões do espaço e dos materiais de revestimento, bem como das atividades que são realizadas no local. Existem tabelas que recomendam o nível de iluminação do ambiente (lux), a relação entre o fluxo luminoso e a área da unidade de sala (lúmen / m²). Nosso aplicativo vai fazer esse cálculo para lhe ajudar a obter a quantidade de lúmen para seu ambiente.'
                 />
 
                 <GlossaryCard 
@@ -36,7 +36,7 @@ const Glossary = () => {
                     border='1px solid gray'
                     upperText='Fluxo Energético ou Potência'
                     bottomText='Watts (w)'
-                    dropdownText='teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste '
+                    dropdownText='É a quantidade de energia elétrica consumida que o equipamento precisa para emitir determinada quantidade de luz (fluxo luminoso). Isso mostra a demanda de instalação de sistema de iluminação elétrica e o consumo elétrico, bem como, a eficiência energética do equipamento. Quanto menos eletricidade o equipamento precisar para a emissão daquela quantidade de fluxo luminoso, maior será sua eficiência energética ou seu rendimento luminoso *** (Lm/W).'
                 />
 
                 <GlossaryCard 
@@ -47,7 +47,7 @@ const Glossary = () => {
                     border='1px solid gray'
                     upperText='Intensidade Luminosa'
                     bottomText='Candela (Cd)'
-                    dropdownText='teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste '
+                    dropdownText='É concentração do fluxo luminoso que se distribui formando um ângulo sólido (o ângulo no espaço tridimensional). Os valores de intensidade luminosa (candela) de uma fonte luminosa são expressos por uma curva, denominada curva de distribuição ou curva zonal. Esta curva depende do ângulo sólido da fonte de luz e da forma do bulbo da lâmpada. Cada tipo de iluminação vai exigir ângulos sólidos específicos e forma de bulbo, de acordo com a necessidade de foco. No caso da luz geral, que demanda uma distribuição mais uniforme possível do fluxo luminoso para proporcionar uma iluminação difusa, a intensidade deve ser sempre a mais baixa possível, sendo recomendado o angulo de abertura do equipamento maior ou igual a 120°. Detalhe! Quando essa informação não estiver na embalagem do produto é porque o angulo é maior ou igual a 120°.'
                 />
 
                 <GlossaryCard 
@@ -58,7 +58,7 @@ const Glossary = () => {
                     border='1px solid gray'
                     upperText='Temperatura de Cor ou Aparência da Cor da Emissão da Luz'
                     bottomText='Kelvin (K)'
-                    dropdownText='teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste '
+                    dropdownText='Temperatura de cor indica a aparência de cor da fonte de luz. Uma temperatura de cor elevada tem uma aparência branca (6500K) e uma baixa temperatura de cor tem uma aparência amarela (3000K). Existem diferentes tipos de branco e amarelo, que são indicados pela unidade Kelvin. Uma aparência de cor neutra está na faixa de 4000K. A aparência branca da luz , considerada uma aparência fria, torna as pessoas mais ativas, enquanto a aparência amarela da luz, considerada uma aparência quente, torna as pessoas mais relaxadas. Em geral, o grupo de lâmpadas com alta temperatura de cor (luz fria) é indicado para espaços com tarefas de precisão. Por outro lado, o grupo de lâmpadas com baixa temperatura de cor (luz quente) é normalmente ideal para espaços de menor concentração, onde o interesse é fazer o lugar aconchegante. No entanto, isso varia de acordo com cada usuário. O importante é não misturar num mesmo acendimento temperaturas de cor distintas. Na duvida, escolha pela luz neutra (4000K).'
                 />
 
                 <GlossaryCard 
@@ -69,7 +69,7 @@ const Glossary = () => {
                     border='1px solid gray'
                     upperText='Índice de Reprodução de Cor ou IRC'
                     bottomText='RA'
-                    dropdownText='teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste '
+                    dropdownText='O indice de reprodução de cor indica quanto a luz da sua fonte (luz artificial) reproduz fielmente as cores de uma objeto ou superficie, comparado com as cores que são percebidas por você quando esse mesmo objeto e superfície é iluminada pela luz do sol (luz natural) ao meio dia. Em uma residência, de modo geral, todas as fontes de luz precisam ter um IRC de bom a excelente, o que varia entre 80RA e 100RA.'
                 />
             </div>
 
