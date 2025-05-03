dedc73d336e81cfbd37f60458b4e9a2f83597839
Pedro Mion <pedrombc@dcc.ufrj.br>
2025-05-03 14:04:04 -0300

    Mudando boards da mainpage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,8 @@
 import React from "react";
 import TopLanding from './TopLanding';
 import BottomLanding from "./BottomLanding";
-import HomeNav from "./HomeNav";
-import navHome from '../Assets/Images/nav-home.svg';
-import navGlossario from '../Assets/Images/nav-glossario.svg';
-import navLightMatch from '../Assets/Images/nav-lightmatch.svg';
-import navFaq from '../Assets/Images/nav-faq.svg';
 import NavBar from "./NavBar/NavBar";
+import './Home.css';
 import { AnimatePresence } from 'framer-motion';
 
 const Home = () => {
